4348e0d3f2458765d27b1b9c1a6ace0a8c7e8c73
bucephalo <seb.jorq@gmail.com>
2022-05-23 09:57:18 -0400

    Avances.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ module.exports = {
       fontFamily:{
         'koulen':['Koulen', 'cursive'],
         'nunito':['Nunito', 'sans-serif'],
-        'poppins':['Poppins', 'sans-serif']
+        'poppins':['Poppins', 'sans-serif'],
+        'robotoMono':['Roboto Mono', 'sans-serif'],
       }
     },
   },
