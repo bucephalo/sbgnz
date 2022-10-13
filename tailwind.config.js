06cbb219394dc308174b4160758c40ad01e78ef3
bucephalo <seb.jorq@gmail.com>
2022-10-13 20:45:29 -0300

    Agrego pestaña Portafolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,17 @@
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily:{
         'koulen':['Koulen', 'cursive'],
         // 'custom':['Open Sans', 'sans-serif'],
-        "fragment-glare": "fragment-glare, serif",
-        "fragment-text": "fragment-text, serif",
+        "fragment-glare": "fragment-glare, sans-serif",
+        "fragment-text": "fragment-text, sans-serif",
         "fragment-sans": "fragment-sans, sans-serif"
       },
       colors: {
         negroColor: 'hsl(0, 0%, 4%)',
         grisColor: 'hsl(222, 10%, 20%)'
-
       }
     },
     borderWidth: {
@@ -22,5 +21,7 @@ module.exports = {
       '6': '6px',
     }
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
